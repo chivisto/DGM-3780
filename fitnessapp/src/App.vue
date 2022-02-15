586aebcbc875a4cc8c2ca05cefac35bcf90c990b
Isaac Covarrubias <49131246+chivisto@users.noreply.github.com>
2022-02-14 17:15:42 -0700

    Add files via upload

---
+++ fitnessapp/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+<img src="../src/assets/running.png">
+<h1>Exercise App</h1>
+  <exerciseForm @add:routine='addRoutine' />
+  <exercise :routines='exerciseDone' />
+</template>
+
+<script>
+import exercise from './components/Exercise.vue'
+import exerciseForm from './components/ExerciseForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    exercise,
+    exerciseForm
+  },
+  data() {
+    return {
+      exerciseDone: [
+      ]
+    }
+  },
+  methods: {
+    addRoutine (routine) {
+      const newEntry = this.exerciseDone.length > 0
+      ? this.exerciseDone[this.exerciseDone.length -1].id
+      : 0;
+      const entry = newEntry + 1;
+      const newRoutines= { ...routine, entry}
+      this.exerciseDone = [ ...this.exerciseDone, newRoutines]
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  color: rgb(25, 85, 25);
+}
+img{
+  width: 200px;
+  height: 200px;
+}
+</style>
---
+++ fitnessapp/src/components/ExerciseForm.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="exercise">
+    <form @submit.prevent="handleSubmit">
+
+      <label>Running or Bench Press? </label>
+      <select v-model="routine.exerciseName">
+        <option>Running</option>
+        <option>Bench Press</option>
+      </select>
+      <br><br><br>
+
+      <h3>Running</h3>
+      <label>Distance in Miles: </label>
+      <input v-model="routine.miles" type="number" />
+
+      <label>Time in Hours: </label>
+      <input v-model="routine.hours" type="number" />
+      <br><br><br>
+
+      <h3>Bench Press</h3>
+      <label>Sets: </label>
+      <input v-model="routine.sets" type="number" />
+
+      <label>Reps: </label>
+      <input v-model="routine.reps" type="number" />
+
+      <label>Weight in Pounds: </label>
+      <input v-model="routine.weight" type="number" />
+
+      <label>Dat: </label>
+      <input v-model="routine.date" type="date" />
+      <br><br><br>
+
+      <button>Add Exercise</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "exercise",
+  data() {
+    return {
+      routine: {
+        exerciseName: "",
+        miles: "",
+        hours: "",
+        sets: "",
+        reps: "",
+        weight: "",
+        date: "",
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit("add:routine", this.routine);
+      this.routine = {
+        exerciseName: "",
+        miles: "",
+        hours: "",
+        sets: "",
+        reps: "",
+        weight: "",
+        date: "",
+      };
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  margin-bottom: 5rem;
+}
+label {
+  padding: 7px;
+}
+button {
+  margin: 5px;
+}
+</style>
